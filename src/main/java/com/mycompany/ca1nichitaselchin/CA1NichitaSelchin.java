d1ce34f64d9c2dca97de773e3cb71b88bece427c
Nichita777 <sba24331@student.cct.ie>
2024-10-27 02:32:01 +0000

    2. Updating Validation Methods
    adding to the code more validation methods 
    validate class
    validate year
    validate purchase

--- src/main/java/com/mycompany/ca1nichitaselchin/CA1NichitaSelchin.java
+++ src/main/java/com/mycompany/ca1nichitaselchin/CA1NichitaSelchin.java
@@ -1,30 +1,89 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.ca1nichitaselchin;
 
-/**
+/* sba24331@student.cct.ie / Nichita Selchin
+ * 
+ * When in opera on, the program will be given a file named “customers.txt” – this contains the details of 
+ * (fic ous) customers in the following format:  
+ * Line 1 – <First Name> <Second Name> 
+ * Line 2 – <Total Purchase> 
+ * Line 3 – <Class>≠ 
+ * Line 4 - <Last Purchase> 
+ * Your task is to:  
+ * 1) Read the data from the file and check that it is valid. The file may contain more than one customer, 
+ * so an appropriate loop should be used. The data must obey the following rules:  
+ * a) the first name must be le ers only;  
+ * b) The second name can be le ers and/or numbers and must be separated from the first 
+ * name by a single space;  
+ * c) the value of purchase of classes must be double and  
+ * d) the Class  must be a integer between 1 to 3. 
+ * e) Last purchase must be a valid year. 
+ * 2) If the data is not valid, you should output a useful error message on screen to the user. 
+ * 3) If the data is valid, then you have to calculate the discount to the net value and save a file named 
+ * customerdiscount.txt, in the following format: 
+ * First name> - <Second Name> 
+ * <Final Value> 
+ * Where the <Final Value> is determined by the number of classes
  *
+ *
+ *
+ * https://github.com/Nichita777/CA1NichitaSelchin  link to GitHub
  * @author nichi
  */
+import java.io.*;
+
 public class CA1NichitaSelchin {
 
+    // Main execution point
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Print the curent working directory to help locate the files
+        System.out.println("Current working directory: " + System.getProperty("user.dir"));
+       // Define input and output file paths
+        String inputFilePath = "customers.txt"; 
+        String outputFilePath = "customerdiscount.txt"; 
+       // process the input file to calculate and write discounts
+        processCustomerFile(inputFilePath, outputFilePath);
     }
-    import java.io.*;
-import java.util.regex.*;
 
-public class CustomerDiscount {
-
-    // Validate first name (letters only)
+    // Validate firstname 
     public static boolean validateFirstName(String firstName) {
         return firstName.matches("[a-zA-Z]+");
     }
 
-    // Validate second name (letters and/or numbers)
+    // Validate secondname 
     public static boolean validateSecondName(String secondName) {
         return secondName.matches("[a-zA-Z0-9]+");
+    }
+    
+    // Validate purchase 
+    public static boolean validatePurchase(String purchase) {
+        try {
+            Double.parseDouble(purchase);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    // Validate class 
+    public static boolean validateClass(String customerClass) {
+        try {
+            int cls = Integer.parseInt(customerClass);
+            return cls >= 1 && cls <= 3;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    // Validate year 
+    public static boolean validateYear(String year) {
+        try {
+            int yr = Integer.parseInt(year);
+            return yr >= 1900 && yr <= 2024;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
